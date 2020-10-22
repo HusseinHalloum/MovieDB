d81565248c3296ecfcc67b4e1be193b00507c7fc
hussein <hseinhalloum00@gmail.com>
2020-10-22 15:04:03 +0300

    Step 4

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.get('/test', (req, res) => res.send({status:200, message:"ok"}))
 
 app.get('/time', (req, res) => res.send({status:200, message:time}))
 
+app.get('/hello/:id' , (req ,res) => {
+    res.status(200).send("hello, " + req.params.id)
+})
 
+app.get('/hello' , (req ,res) => {
+    res.status(200).send("hello")
+})
+
+app.get('/search', (req, res) => {
+    const {s} = req.query;
+    if(!s){
+        res.send({status:500, error:true, message:"you have to provide a search"})
+    }
+    else {
+         res.send({status:200, message:"ok", data:s})
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
